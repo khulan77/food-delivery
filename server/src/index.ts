0155d1cab0c6c2399869cbf59cf44f4e99de11ab
25LP7865 25LP7865 <dhulan174@gmail.com>
2026-01-28 17:04:13 +0800

    djefjhebdc

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("index");
+import express from "express";
+import cors from "cors";
+import { configDotenv } from "dotenv";
+import connectToMongoDB from "./mongodb";
+
+configDotenv();
+
+const app = express();
+connectToMongoDB();
+app.use(cors());
+app.use(express.json());
+
+app.listen(8000, () => {
+  console.log("server running on http://localhost:8000");
+});
